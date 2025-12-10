cfc7503e2efc31acb98c137a74c6218908eacf5e
Haseeb-u-Rehman <haseeb16001@gmail.com>
2025-12-10 11:11:07 +0500

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,14 @@
+import logging
+import os
+from datetime import datetime
+LOGFILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
+log_path =os.path.join(os.getcwd(),"logs",LOGFILE)
+os.makedirs(log_path,exist_ok=True)
+LOGFILEPATH=os.path.join(log_path,LOGFILE)
+
+logging.basicConfig(
+    filename=LOGFILEPATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
+
+
