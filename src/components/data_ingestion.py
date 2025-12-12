77b4c7075a929e17c0a0e0ccb5b658696f81d91b
Haseeb-u-Rehman <haseeb16001@gmail.com>
2025-12-12 09:53:38 +0500

    model_train and hyperparamet tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ import sys
 import pandas as pd
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import datatransformation
+from src.components.data_transformation import dataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class dataIngestionConfig:
@@ -35,7 +38,15 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
             
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
-            
+
+
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_Transformation=datatransformation()
+    train_arr,test_arr,_=data_Transformation.initiate_data_transformation(train_data,test_data)
+    model_train = ModelTrainer()
+    print(model_train.initiate_model_trainer(train_array=train_arr,test_array=test_arr))
+      
