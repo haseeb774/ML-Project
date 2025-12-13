cd7245b7bf6419b260e3a8b4d9acf821fc32df5b
Haseeb-u-Rehman <haseeb16001@gmail.com>
2025-12-13 22:25:13 +0500

    save for dployment

--- application.py
+++ application.py
@@ -28,6 +28,7 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
+            
 
         )
         pred_df=data.get_data_as_data_frame()
